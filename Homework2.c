a1b06705e9a8271abbc965634614d7e7de890ef4
mohamedsawan <56001945+mohamedsawan@users.noreply.github.com>
2019-11-12 03:48:58 -0600

    Update Homework2.c

--- Homework2.c
+++ Homework2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <X11/Xlib.h>
-int vertical [3][3] = { { -1 , 0 , 1 } , { -1 , 0 , 1 } , { - 1 , 0 , 1 } };
-int horizontal [3][3] = { { -1 , -1 , -1 } , { 0 , 0 , 0 } , { 1 , 1 , 1 } };
+int horizontal [3][3] = { { -1 , 0 , 1 } , { -1 , 0 , 1 } , { - 1 , 0 , 1 } };
+int vertical [3][3] = { { -1 , -1 , -1 } , { 0 , 0 , 0 } , { 1 , 1 , 1 } };
 
 
 #define DIM 512
-#define VERTICAL 1
+#define VERTICAL 0
 int convolve(unsigned char image[DIM][DIM], int i, int j)
 {
     int a, b, sum = 0;
@@ -23,12 +23,31 @@ int convolve(unsigned char image[DIM][DIM], int i, int j)
       }
     }
     return sum;
+}
+/******************************************************************/
+/* This structure contains the coordinates of a box drawn with    */
+/* the left mouse button on the image window.                     */
+/* roi.x , roi.y  - left upper corner's coordinates               */
+/* roi.width , roi.height - width and height of the box           */
+/******************************************************************/
+extern XRectangle roi;
+
+
+/******************************************************************/
+/* Main processing routine. This is called upon pressing the      */
+/* Process button of the interface.                               */
+/* image  - the original greyscale image                          */
+/* size   - the actual size of the image                          */
+/* proc_image - the image representation resulting from the       */
+/*              processing. This will be displayed upon return    */
+/*              from this function.                               */
+/******************************************************************/
 void process_image(image, size, proc_img)
 unsigned char image[DIM][DIM];
 int size[2];
 unsigned char proc_img[DIM][DIM];
 {
-  int i, j;
+  int i , j;
   for(i = 1; i < DIM-1; i++)
   {
     for(j = 1; j < DIM-1; j++)
