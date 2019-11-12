fd1dc51d4fd2857e74adaf3f390ab03c1310dff9
mohamedsawan <56001945+mohamedsawan@users.noreply.github.com>
2019-11-12 03:36:25 -0600

    Update Homework2.c

--- Homework2.c
+++ Homework2.c
@@ -7,40 +7,36 @@ int horizontal [3][3] = { { -1 , -1 , -1 } , { 0 , 0 , 0 } , { 1 , 1 , 1 } };
 
 #define DIM 512
 #define VERTICAL 1
-int convolve(unsigned char image[DIM][DIM], int arr[3][3], int i, int j)
+int convolve(unsigned char image[DIM][DIM], int i, int j)
 {
-    int a, b, sum;
+    int a, b, sum = 0;
+    int vertical [3][3] = { { -1 , 0 , 1 } , { -1 , 0 , 1 } , { - 1 , 0 , 1 } };
+    int horizontal [3][3] = { { -1 , -1 , -1 } , { 0 , 0 , 0 } , { 1 , 1 , 1 } };
     for(a = 0; a < 3; a++)
     {
       for(b = 0; b < 3; b++)
       {
-        sum += image[i + 1 - a][j + 1 - b] * arr[i][j];
+        if(VERTICAL)
+          sum += image[i + 1 - a][j + 1 - b] * vertical[a][b];
+        else
+          sum += image[i + 1 - a][j + 1 - b] * horizontal[a][b];
       }
     }
-}
-
+    return sum;
 void process_image(image, size, proc_img)
 unsigned char image[DIM][DIM];
 int size[2];
 unsigned char proc_img[DIM][DIM];
 {
   int i, j;
-  for(i = 0; i < DIM-1; i++)
+  for(i = 1; i < DIM-1; i++)
   {
-    for(j = 0; j < DIM-1; j++)
+    for(j = 1; j < DIM-1; j++)
     {
-      if(VERTICAL)
-        proc_img[i][j] = abs(convolve(image, vertical, i, j))/3;
-      else
-        proc_img[i][j] = abs(convolve(image, horizontal, i , j))/3;
+      proc_img[i][j] = abs(convolve(image, i, j))/3;
     }
   }
-
+  size[0] = 510;
+  size[1] = 510;
 }
 
-
-
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
