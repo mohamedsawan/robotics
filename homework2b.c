0017fd7799c2465893cd2350a928b628a194ffef
mohamedsawan <56001945+mohamedsawan@users.noreply.github.com>
2019-11-13 18:51:02 -0600

    Update homework2b.c

--- homework2b.c
+++ homework2b.c
@@ -4,43 +4,89 @@
 #define VERTICAL 1
 
 
-int convolve(unsigned char image[DIM][DIM], int i, int j)
+int convolve(unsigned char image[DIM][DIM], int temp [roi.height][roi.length], int x, int y, int avg, int tAvg, StDev, tStDev)
 {
-    int a, b, sum = 0;
+    int X, Y, sum = 0;
 
     int t[5][5];
-    for(a = 0; a < 3; a++)
+    
+    for(X = 0; X < roi.width; X++)
     {
-      for(b = 0; b < 3; b++)
+      for(Y = 0; Y < roi.height; Y++)
       {
-        sum += ((image[i + 1 - a][j + 1 - b] - avg) * (vertical[a][b] - tAvg))/(StDev*tStDev);
+        sum += (image[y + Y][x + X] - avg) * (temp[Y][X] - tAvg);
       }
     }
-    return sum;
+    return sum/(StDev*tStDev);
 }
+/******************************************************************/
+/* This structure contains the coordinates of a box drawn with    */
+/* the left mouse button on the image window.                     */
+/* roi.x , roi.y  - left upper corner's coordinates               */
+/* roi.width , roi.height - width and height of the box           */
+/******************************************************************/
+extern XRectangle roi;
+
+
+/******************************************************************/
+/* Main processing routine. This is called upon pressing the      */
+/* Process button of the interface.                               */
+/* image  - the original greyscale image                          */
+/* size   - the actual size of the image                          */
+/* proc_image - the image representation resulting from the       */
+/*              processing. This will be displayed upon return    */
+/*              from this function.                               */
+/******************************************************************/
 
 
 int main(void) {
-  int i, j, xlength, ylength, sum = 0, avg, variance = 0, StDev;
-  for(i = 0; i < xlength; i++)
+  int x, y, xlength, ylength, sum = 0, tSum = 0, avg, tAvg, variance = 0, StDev, best = 0;
+  int temp [roi.height][roi.width];
+  for(x = 0; x < roi.width; x++)
   {
-    for(j = 0; j < ylength; j++)
+      for(y = 0; y < roi.height; y++)
+      {
+          temp[y][x] = image[roi.x + x][roi.y + y];
+          tSum += temp[y][x];
+      }
+  }
+  tAvg = tSum/(roi.width*roi.length);
+   
+   for(x = 0; x < roi.width; x++)
+  {
+      for(y = 0; y < roi.length; y++)
+      {
+          best += temp[y][x] * temp[y][x];
+      }
+  }
+          
+  for(x = 0; x < DIM; x++)
+  {
+    for(y = 0; y < DIM; y++)
     {
-      sum += image[j][i];
+      sum += image[y][x];
     }
   }
   
-  avg = sum/(xlength*ylength);
+  avg = sum/(DIM*DIM);
 
-  for(i = 0; i < xlength; i++)
+  for(x = 0; x < DIM; x++)
   {
-    for(j = 0; j < ylength; j++)
+    for(y = 0; y < DIM; y++)
     {
-      variance += pow(image[j][i] - avg,2);
+      variance += pow(image[y][x] - avg,2);
     }
   }
 
   StDev = pow(variance, 0.5);
+  
+  for(x = 0; x < DIM-roi.width; x++)
+  {
+    for(y = 0; y < DIM-roi.height; y++)
+    {
+      proc_img[y][x] = abs(convolve(image, x, y))/3;
+    }
+  }
 
   
 }
